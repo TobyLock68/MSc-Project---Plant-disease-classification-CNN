8c65caeedd163d47af0ab3eb0771d45d8669737f
TobyLock68 <tobylock68@gmail.com>
2026-05-03 18:06:38 +0100

    Update distortion_layer_2.py

---
+++ coded_parts/distortion_layer_2.py
@@ -0,0 +1,18 @@
+import albumentations as album
+import random
+import numpy as np
+
+#image fog function
+
+def add_fog(image):
+
+
+#image lens flare function
+
+def add_lens_flare(image):
+
+
+#combining layer 2 distortions
+
+def apply_layer_2(image):
+    
