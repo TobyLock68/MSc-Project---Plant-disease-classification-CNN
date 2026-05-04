e17157cd9af9a3597e9ca67c86522c3880fb165f
TobyLock68 <tobylock68@gmail.com>
2026-05-04 14:59:21 +0100

    Update distortion_layer_2.py

--- coded_parts/distortion_layer_2.py
+++ coded_parts/distortion_layer_2.py
@@ -5,6 +5,13 @@ import numpy as np
 #image fog function
 
 def add_fog(image):
+    transform = album.Compose([
+        album.RandomFog(fog_coef_lower = 0.2, fog_coef_upper = 0.4, alpha_coef = 0.4, p = 1.0)
+        ])
+    foggy_image = transform(image = image)['image']
+    return foggy_image
+                              
+
 
 
 #image lens flare function
