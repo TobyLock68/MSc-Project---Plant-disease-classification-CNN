f2456c3c42393cbb10ac2081f47e7849b264126d
TobyLock68 <tobylock68@gmail.com>
2026-05-22 13:56:10 +0100

    Update baseline_training.py
    data preparation and cross val start

--- training/baseline_training.py
+++ training/baseline_training.py
@@ -17,4 +17,22 @@ model_save_dir.mkdir(exist_ok=True)
 #checks if better gpu hardware available (scalable not useful for me)
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 
-#data preparation:
+#data/ image preparation:
+img_transformation = transforms.Compose([
+    transforms.Resize((224,224)),    #makes images all standard size
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225])     #ImageNet dataset exact averages
+])
+
+#data loading
+data = datasets.ImageFolder(root = str(plantvil_dir), transform = img_transformation)
+num_labels = [sample[1] for sample in data.samples]
+
+#cross val 
+#splitting logic
+skf = StratifiedKFold(n_splits = 5, shuffle = True, random_state = 42)
+    #specifies no. splits, randomisation of data (we are in alphabetical order) and set seed
+
+
+fold_accuracies = []    #stores accuracies to given baseline at end
+
