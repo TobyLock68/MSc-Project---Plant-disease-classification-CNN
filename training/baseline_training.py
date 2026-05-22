f83be1b669f8fad8ddfbeb220a28e304fd9ec7c1
TobyLock68 <tobylock68@gmail.com>
2026-05-22 14:43:35 +0100

    Update baseline_training.py
    finishing cross val section and setting up the model.

--- training/baseline_training.py
+++ training/baseline_training.py
@@ -36,3 +36,34 @@ skf = StratifiedKFold(n_splits = 5, shuffle = True, random_state = 42)
 
 fold_accuracies = []    #stores accuracies to given baseline at end
 
+#looping cross val
+fold_splits = skf.split(X=np.zeros(len(num_labels)), y = num_labels)
+
+for fold, (train_idx, val_idx) in enumerate(fold_splits, start = 1):
+
+    #creates the data subsets
+    train_subset = Subset(data, train_idx)
+    validation_subset = Subset(data, val_idx)
+
+    #preps data subsets to be fed into model
+    train_loader = DataLoader(train_subset, batch_size=32, shuffle=True)
+    validation_loader = DataLoader(validation_subset, batch_size=32, shuffle=False)
+
+
+#model setup
+model = models.resnet50(weights = model.ResNet50_Weights.DEFAULT)
+
+#freezing of convolutional layers 
+for param in model.parameters():
+    param.requires_grad = False
+
+#global average pooling 
+in_features = model.fc.in_features
+
+#replacement of 1000-output layer to our max of 38 classes and apply softmax
+model.fc = nn.Linear(in_features, 38)
+
+model = model.to(device)    #sends to previously defined GPU if available
+
+optimizer = optim.Adam(model.fc.parameters(), lr=0.001)     #targeted training and learning algorithm
+criteria = nn.CrossEntropyLoss()    #loss function (how wrong models guess was)
