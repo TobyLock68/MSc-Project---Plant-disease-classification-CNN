f485c770dd4c2b351c918d08548af397f72c062a
TobyLock68 <tobylock68@gmail.com>
2026-06-01 12:46:12 +0100

    Create model_testing.py
    code carry over from training page with handy function to run training weights into the model

---
+++ training/model_testing.py
@@ -0,0 +1,49 @@
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+
+#same starting code as training with slight name change
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
+test_transformation = transforms.Compose([
+    transforms.Resize((224,224)),    #makes images all standard size
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225])     #ImageNet dataset exact averages
+])
+
+#data loading
+plantdoc_data = ImageFolder(root="../data/PlantDoc-Dataset", transform=test_transformation)
+test_loader = DataLoader(plantdoc_data, batch_size = 32, shuffle = False)
+
+#function to load and test model weights
+def model_eval(weight_file_path):
+    #shortened model setup as we are passing weights to the model from training
+    model = models.resnet50(weights = None)
+    model.fc = torch.nn.Linear(model.fc.in_features, 38)
+
+    model.load_state_dict(torch.load(f"../models/{weight_file_path}", map_location=device))
+    model = model.to(device)
+    model.eval()
+
+    #setting up of variables to track accuracy
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for inputs, labels in test_loader:          #begins loop pf batched images
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)         #forward pass
+            _, preds = torch.max(outputs, 1)        #disctates the class/prediction
+            correct += (preds == labels).sum().item()       #compares prediciton to actual label and counts correct
+            total += labels.size(0)
+    
+    accuracy = (correct/total)*100
+
+    print(f"Accuracy for {weight_file_path} on PlantDoc test = {accuracy:.2f}%")
+    return accuracy
+
+#run baseline and augemented in parallel
+baseline_test_acc = model_eval("baseline_training_accuracies.pth")
+augmented_test_acc = model_eval("augmented_training_accuracies.pth")
