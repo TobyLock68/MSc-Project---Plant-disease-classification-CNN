b8927e2a7dd794302af993d7d45c83adcae63b54
TobyLock68 <tobylock68@gmail.com>
2026-06-22 14:58:13 +0100

    Update model_testing.py
    update after missing mapping of class names to to test script

--- training/model_testing.py
+++ training/model_testing.py
@@ -1,8 +1,11 @@
 import torch
+import os
+from pathlib import Path
 import torchvision.models as models
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
+from dataset_dictionary import PLANTDOC_TO_PLANTVILLAGE
 
 #same starting code as training with slight name change
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
@@ -13,9 +16,31 @@ test_transformation = transforms.Compose([
     transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225])     #ImageNet dataset exact averages
 ])
 
+#directories
+root = Path(__file__).parent.parent
+plantvil_dir = root/"data" / "plantvillage dataset" / "color"
+plantdoc_dir = root/"data" / "PlantDoc-Dataset"
+
 #data loading
-plantdoc_data = ImageFolder(root="../data/PlantDoc-Dataset", transform=test_transformation)
+plantdoc_data = ImageFolder(root=str(plantdoc_dir), transform=test_transformation)
 test_loader = DataLoader(plantdoc_data, batch_size = 32, shuffle = False)
+
+#index the dictionary
+pv_classes_sort = sorted([d for d in os.listdir(plantvil_dir) if os.path.isdir(plantvil_dir / d) and not d.startswith('.')])
+class_to_idx = {name: idx for idx, name in enumerate(pv_classes_sort)}
+
+pd_to_pv_idx = {}
+
+for pd_idx, pd_name in enumerate(plantdoc_data.classes):
+    if pd_name in PLANTDOC_TO_PLANTVILLAGE:
+        matched = PLANTDOC_TO_PLANTVILLAGE[pd_name]
+        if matched in class_to_idx:
+            pd_to_pv_idx[pd_idx] = class_to_idx[matched]
+        else:
+            pd_to_pv_idx[pd_idx] = -1
+    else:
+        pd_to_pv_idx[pd_idx] = -1
+
 
 #function to load and test model weights
 def model_eval(weight_file_path):
@@ -36,10 +61,14 @@ def model_eval(weight_file_path):
             inputs, labels = inputs.to(device), labels.to(device)
             outputs = model(inputs)         #forward pass
             _, preds = torch.max(outputs, 1)        #disctates the class/prediction
-            correct += (preds == labels).sum().item()       #compares prediciton to actual label and counts correct
-            total += labels.size(0)
+            mapped_labels = torch.tensor([pd_to_pv_idx[l.item()] for l in labels]).to(device)
+            valid = mapped_labels != -1
+            
+            if valid.sum() > 0:
+                correct += (preds[valid] == mapped_labels[valid]).sum().item()
+                total += valid.sum().item()
     
-    accuracy = (correct/total)*100
+    accuracy = (correct/total)*100 if total > 0 else 0.0
 
     print(f"Accuracy for {weight_file_path} on PlantDoc test = {accuracy:.2f}%")
     return accuracy
