33005d59fd2aed3e61f81d25c1dd75e3d18da786
TobyLock68 <tobylock68@gmail.com>
2026-07-03 16:06:29 +0100

    Update model_training.py
    thread protection

--- training/model_training.py
+++ training/model_training.py
@@ -46,95 +46,96 @@ img_transformation = transforms.Compose([
     transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225])     #ImageNet dataset exact averages
 ])
 
-#data loading
-data = datasets.ImageFolder(root = str(plantvil_dir), transform = img_transformation)
-num_labels = [sample[1] for sample in data.samples]
+if __name__ == "__main__":
+    #data loading
+    data = datasets.ImageFolder(root = str(plantvil_dir), transform = img_transformation)
+    num_labels = [sample[1] for sample in data.samples]
 
-#cross val 
-#splitting logic
-skf = StratifiedKFold(n_splits = 5, shuffle = True, random_state = 42)
-    #specifies no. splits, randomisation of data (we are in alphabetical order) and set seed
+    #cross val 
+    #splitting logic
+    skf = StratifiedKFold(n_splits = 5, shuffle = True, random_state = 42)
+        #specifies no. splits, randomisation of data (we are in alphabetical order) and set seed
 
 
-fold_accuracies = []    #stores accuracies to given baseline at end
+    fold_accuracies = []    #stores accuracies to given baseline at end
 
-#looping cross val
-fold_splits = skf.split(X=np.zeros(len(num_labels)), y = num_labels)
+    #looping cross val
+    fold_splits = skf.split(X=np.zeros(len(num_labels)), y = num_labels)
 
-for fold, (train_idx, val_idx) in enumerate(fold_splits, start = 1):
-    print(f"K-Fold Progress: Fold {fold}/5 ({experiment_name})")
+    for fold, (train_idx, val_idx) in enumerate(fold_splits, start = 1):
+        print(f"K-Fold Progress: Fold {fold}/5 ({experiment_name})")
 
-    #creates the data subsets
-    train_subset = Subset(data, train_idx)
-    validation_subset = Subset(data, val_idx)
+        #creates the data subsets
+        train_subset = Subset(data, train_idx)
+        validation_subset = Subset(data, val_idx)
 
-    #preps data subsets to be fed into model
-    train_loader = DataLoader(train_subset, batch_size=32, shuffle=True, num_workers=4, pin_memory=True)
-    validation_loader = DataLoader(validation_subset, batch_size=32, shuffle=False, num_workers=4, pin_memory=True)
-    
-    #model setup
-    model = models.resnet50(weights = models.ResNet50_Weights.DEFAULT)
-
-    #freezing of convolutional layers 
-    for param in model.parameters():
-        param.requires_grad = False
-
-    #global average pooling 
-    in_features = model.fc.in_features
-
-    #replacement of 1000-output layer to our max of 38 classes and apply softmax
-    model.fc = nn.Linear(in_features, 38)
-
-    model = model.to(device)    #sends to previously defined GPU if available
-
-    optimiser = optim.Adam(model.fc.parameters(), lr=0.001)     #targeted training and learning algorithm
-    criteria = nn.CrossEntropyLoss()    #loss function (how wrong models guess was)
-
-    #--------- ACTTUAL TRAINING CODE -------------
-
-    best_validation_accuracy = 0.0
-    best_weights = copy.deepcopy(model.state_dict())
-
-    for epoch in range (1,16):      #15 training epochs for each fold as per literature
-        model.train()
-
-        for batch_idx, (inputs, batch_labels) in enumerate(train_loader, start=1):
-            inputs, batch_labels = inputs.to(device, non_blocking=True), batch_labels.to(device, non_blocking=True)
-
-            optimiser.zero_grad()       #removes previous gradients
-            outputs = model(inputs)     #forward pass
-            loss =criteria(outputs, batch_labels)
-            loss.backward()     #backward pass
-            optimiser.step()        #update weights
-
-            if batch_idx % 100 == 0 or batch_idx == len(train_loader):
-                print(f" EPOCH = {epoch}/15 --- Batch {batch_idx}/{len(train_loader)}")
-
-            #validation stage within the training loop
-
-        model.eval()
-        validation_correct = 0
-        with torch.no_grad():
-            for inputs, batch_labels in validation_loader:
-                inputs, batch_labels = inputs.to(device), batch_labels.to(device)
-                outputs = model(inputs)
-                _, preds = torch.max(outputs, 1)
-                validation_correct += torch.sum(preds ==  batch_labels.data)
+        #preps data subsets to be fed into model
+        train_loader = DataLoader(train_subset, batch_size=32, shuffle=True, num_workers=4, pin_memory=True)
+        validation_loader = DataLoader(validation_subset, batch_size=32, shuffle=False, num_workers=4, pin_memory=True)
         
-        epoch_accuracy = (validation_correct.double() / len(val_idx)).item()
+        #model setup
+        model = models.resnet50(weights = models.ResNet50_Weights.DEFAULT)
 
-        print(f" Epoch {epoch}/15 | Accuracy = {epoch_accuracy:.4f}")
+        #freezing of convolutional layers 
+        for param in model.parameters():
+            param.requires_grad = False
 
-        #tracking the best weights for each fold
-        if epoch_accuracy > best_validation_accuracy:
-            best_validation_accuracy = epoch_accuracy
-            best_weights = copy.deepcopy(model.state_dict())
+        #global average pooling 
+        in_features = model.fc.in_features
 
-    fold_accuracies.append(best_validation_accuracy)
+        #replacement of 1000-output layer to our max of 38 classes and apply softmax
+        model.fc = nn.Linear(in_features, 38)
 
-    model_filename = f"resnet50_{experiment_name}_fold_{fold}.pth"
-    torch.save(best_weights, model_save_dir / model_filename)
-    print(f"Optimal weights for fold saved: models/{model_filename}")
+        model = model.to(device)    #sends to previously defined GPU if available
+
+        optimiser = optim.Adam(model.fc.parameters(), lr=0.001)     #targeted training and learning algorithm
+        criteria = nn.CrossEntropyLoss()    #loss function (how wrong models guess was)
+
+        #--------- ACTTUAL TRAINING CODE -------------
+
+        best_validation_accuracy = 0.0
+        best_weights = copy.deepcopy(model.state_dict())
+
+        for epoch in range (1,16):      #15 training epochs for each fold as per literature
+            model.train()
+
+            for batch_idx, (inputs, batch_labels) in enumerate(train_loader, start=1):
+                inputs, batch_labels = inputs.to(device, non_blocking=True), batch_labels.to(device, non_blocking=True)
+
+                optimiser.zero_grad()       #removes previous gradients
+                outputs = model(inputs)     #forward pass
+                loss =criteria(outputs, batch_labels)
+                loss.backward()     #backward pass
+                optimiser.step()        #update weights
+
+                if batch_idx % 100 == 0 or batch_idx == len(train_loader):
+                    print(f" EPOCH = {epoch}/15 --- Batch {batch_idx}/{len(train_loader)}")
+
+                #validation stage within the training loop
+
+            model.eval()
+            validation_correct = 0
+            with torch.no_grad():
+                for inputs, batch_labels in validation_loader:
+                    inputs, batch_labels = inputs.to(device), batch_labels.to(device)
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    validation_correct += torch.sum(preds ==  batch_labels.data)
+            
+            epoch_accuracy = (validation_correct.double() / len(val_idx)).item()
+
+            print(f" Epoch {epoch}/15 | Accuracy = {epoch_accuracy:.4f}")
+
+            #tracking the best weights for each fold
+            if epoch_accuracy > best_validation_accuracy:
+                best_validation_accuracy = epoch_accuracy
+                best_weights = copy.deepcopy(model.state_dict())
+
+        fold_accuracies.append(best_validation_accuracy)
+
+        model_filename = f"resnet50_{experiment_name}_fold_{fold}.pth"
+        torch.save(best_weights, model_save_dir / model_filename)
+        print(f"Optimal weights for fold saved: models/{model_filename}")
 
 
-print(f"Training finished. Average accuracy = {np.mean(fold_accuracies):.4f}\n")
+    print(f"Training finished. Average accuracy = {np.mean(fold_accuracies):.4f}\n")
