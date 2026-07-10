a82e2a1652779d0e11be5a06c3b24b51207bc816
TobyLock68 <tobylock68@gmail.com>
2026-07-10 17:31:51 +0100

    Update model_testing.py
    changing testing to cover ensemble and best fold appraoch as well as adding colab workaround

--- training/model_testing.py
+++ training/model_testing.py
@@ -10,6 +10,8 @@ from torch.utils.data import DataLoader
 from dataset_dictionary import PLANTDOC_TO_PLANTVILLAGE
 from sklearn.metrics import precision_recall_fscore_support
 
+#same starting code as training with slight name change
+
 def seed_reproducibility(seed=42):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
@@ -17,11 +19,37 @@ def seed_reproducibility(seed=42):
     torch.manual_seed(seed)
     if torch.backends.mps.is_available():
         torch.mps.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
 
 seed_reproducibility(42)
 
-#same starting code as training with slight name change
-device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+# ---- COLAB SETUP ---
+root_dir = Path("/content")
+plantdoc_dir = root_dir / "PlantDoc-Dataset"
+plantvil_dir = root_dir/"plantvillage dataset"/ "color"
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if torch.cuda.is_available():
+    torch.backends.cudnn.benchmark = True
+num_workers_cfg = 2
+
+# --- LOCAL SETUP ---
+# root = Path(__file__).parent.parent
+# plantdoc_dir = root/"data" / "PlantDoc-Dataset"
+# plantvil_dir = root_dir/"data"/"plantvillage dataset"/ "color"
+# device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+# num_workers_cfg = 0
+
+model_save_dir = root_dir/"models"
+model_save_dir.mkdir(exist_ok=True)
+
+#best fold for each training run determined by average fold accuracy
+baseline_best_fold = 5
+augemented_best_fold = 2
+
+# --- Data setup ---
 
 test_transformation = transforms.Compose([
     transforms.Resize((224,224)),    #makes images all standard size
@@ -29,14 +57,9 @@ test_transformation = transforms.Compose([
     transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225])     #ImageNet dataset exact averages
 ])
 
-#directories
-root = Path(__file__).parent.parent
-plantvil_dir = root/"data" / "plantvillage dataset" / "color"
-plantdoc_dir = root/"data" / "PlantDoc-Dataset"
-
 #data loading
 plantdoc_data = ImageFolder(root=str(plantdoc_dir), transform=test_transformation)
-test_loader = DataLoader(plantdoc_data, batch_size = 32, shuffle = False)
+test_loader = DataLoader(plantdoc_data, batch_size = 32, shuffle = False, num_workers=num_workers_cfg)
 
 #index the dictionary
 pv_classes_sort = sorted([d for d in os.listdir(plantvil_dir) if os.path.isdir(plantvil_dir / d) and not d.startswith('.')])
@@ -56,27 +79,65 @@ for pd_idx, pd_name in enumerate(plantdoc_data.classes):
 
 
 #function to load and test model weights
-def model_eval(weight_file_path):
+def model_eval(experiment, best_fold_idx):
+    '''
+    Loads all of our model folds for each experiment and runs an ensemble method, taking average weights across
+    all folds, and an isolated method, using the fold with the highest accuracy.
+    '''
     #shortened model setup as we are passing weights to the model from training
-    model = models.resnet50(weights = None)
-    model.fc = torch.nn.Linear(model.fc.in_features, 38)
+    model_list = []
 
-    model.load_state_dict(torch.load(f"../models/{weight_file_path}", map_location=device))
-    model = model.to(device)
-    model.eval()
+    for i in range(1,6):
+        file_name = f"{experiment}_fold_{i}.pth"
+        checkpoint_path = model_save_dir / file_name
 
-    #setting up of variables to track metrics
-    all_preds = []
+        if not checkpoint_path.exists():
+            raise FileNotFoundError("Couldn't load weights")
+        
+        model = models.resnet50(weights = None)
+        model.fc = torch.nn.Linear(model.fc.in_features, 38)
+
+        model.load_state_dict(torch.load(checkpoint_path, map_location=device))
+        model.to(device)
+        model.eval()
+        model_list.append(model)
+
+# -- Best Fold Isolated Approach --
+
+    #identify best fold
+    best_model = model_list[best_fold_idx - 1]
+
+    #metric calculation tracking variables
+    best_fold_preds = []
+    ensemble_preds = []
     targets = []
 
     with torch.no_grad():
         for inputs, labels in test_loader:          #begins loop pf batched images
             inputs, labels = inputs.to(device), labels.to(device)
-            outputs = model(inputs)         #forward pass
-            _, preds = torch.max(outputs, 1)        #disctates the class/prediction
+            batch_size = inputs.size(0)
 
             mapped_labels = torch.tensor([pd_to_pv_idx[l.item()] for l in labels]).to(device)
             valid = mapped_labels != -1
+
+            if valid.sum() > 0:
+                best_outputs = best_model(inputs)
+                _, b_preds = torch.max(best_outputs, 1)
+
+                # --- Ensemble approach ----
+
+                accum_probs = torch.zeros((batch_size, 38), device=device)
+                for model in model_list:
+                    outputs = model(inputs)
+
+
+
+
+
+            outputs = model(inputs)         #forward pass
+            _, preds = torch.max(outputs, 1)        #disctates the class/prediction
+
+
             
             if valid.sum() > 0:
                 all_preds.extend(preds[valid].cpu().numpy())
